ddbf3bc34a23c35056c7f26eeec236edfb67448d
jimmy <836081346@qq.com>
2021-06-27 18:43:33 +0800

    Updates

---
+++ precache-manifest.806ab4c239ba0360c77ee942b13b599d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1635e74e858e208dcbc1075e3acc050",
+    "url": "/MuiBurgerBuilder/index.html"
+  },
+  {
+    "revision": "e1e9ca49852cc6dcc75c",
+    "url": "/MuiBurgerBuilder/static/css/main.d723e454.chunk.css"
+  },
+  {
+    "revision": "024434d575924b4a69a6",
+    "url": "/MuiBurgerBuilder/static/js/2.b9fc88cc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MuiBurgerBuilder/static/js/2.b9fc88cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e9ca49852cc6dcc75c",
+    "url": "/MuiBurgerBuilder/static/js/main.4c650f80.chunk.js"
+  },
+  {
+    "revision": "98c733b837f9e3f275f5",
+    "url": "/MuiBurgerBuilder/static/js/runtime-main.80ed2c6b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/MuiBurgerBuilder/static/media/27.1 burger-logo.png.b8503d26.png"
+  }
+]);
